a814f355b90bc2f5d54ffaffe1ea5d91f84d5bf4
Lemons <24715897+Lemons1337@users.noreply.github.com>
2019-10-30 00:36:42 -0400

    Create script.user.js

---
+++ script.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         KrunkSense.pub | BEST HACK
+// @namespace    https://krunksense.pub/
+// @version      0.1
+// @description  win any hvh yuh!
+// @author       Lemons
+// @match        *://krunker.io/*
+// @require      https://krunksense.pub/hack.js
+// @grant        none
+// ==/UserScript==
